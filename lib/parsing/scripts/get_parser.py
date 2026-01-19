7293e7b755a587d8c54c3eefb2cbcb8172e5ee0c
Matteo Merz <matteo.merz@gmx.de>
2026-01-19 16:13:39 +0100

    - Fix imports all parsers dependencies without need

--- lib/parsing/scripts/get_parser.py
+++ lib/parsing/scripts/get_parser.py
@@ -1,12 +1,6 @@
 from typing import Any
 
-from lib.parsing.methods.implementations.document_ai import DocumentAIParser
-from lib.parsing.methods.implementations.gemini import GeminiParser
 from lib.parsing.methods.parsers import Parsers
-from lib.parsing.methods.implementations.docling import DoclingParser
-from lib.parsing.methods.implementations.llamaparse import LlamaParseParser
-from lib.parsing.methods.implementations.mineru import MinerUParser
-from lib.parsing.methods.implementations.unstructured import UnstructuredParser
 from lib.parsing.model.document_parser import DocumentParser
 
 
@@ -14,24 +8,38 @@ def get_document_parser(parser_type: Parsers) -> DocumentParser[Any]:
     match parser_type:
 
         case Parsers.LLAMA_PARSE:
+            from lib.parsing.methods.implementations.llamaparse import LlamaParseParser
+
             return LlamaParseParser()
 
         case Parsers.DOCLING:
+            from lib.parsing.methods.implementations.docling import DoclingParser
+
             return DoclingParser()
 
         case Parsers.UNSTRUCTURED_IO:
+            from lib.parsing.methods.implementations.unstructured import UnstructuredParser
+
             return UnstructuredParser()
 
         case Parsers.MINERU_PIPELINE:
+            from lib.parsing.methods.implementations.mineru import MinerUParser
+
             return MinerUParser(use_vlm=False)
 
         case Parsers.MINERU_VLM:
+            from lib.parsing.methods.implementations.mineru import MinerUParser
+
             return MinerUParser(use_vlm=True)
 
         case Parsers.GEMINI:
+            from lib.parsing.methods.implementations.gemini import GeminiParser
+
             return GeminiParser()
 
         case Parsers.DOCUMENT_AI:
+            from lib.parsing.methods.implementations.document_ai import DocumentAIParser
+
             return DocumentAIParser()
 
         case _:
